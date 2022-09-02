204caf37a14a3d3e144c831f825841f05f0601a4
remon <blackshadowsoftwareltd@gmail.com>
2022-09-03 00:56:38 +0600

    reciepe image

--- lib/src/screens/recipe/components/image.dart
+++ lib/src/screens/recipe/components/image.dart
@@ -5,12 +5,24 @@ class RecipeImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SliverToBoxAdapter(
-      child: Card(
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
         child: SizedBox(
-          height: 200.0,
+          width: MediaQuery.of(context).size.width,
+          height: 180,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Image.network(
+              img,
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
       ),
     );
   }
 }
+
+const img =
+    'https://www.howsweeteats.com/wp-content/uploads/2016/08/bacon-avo-egg-toast-I-howsweeteats.com-9.jpg';
